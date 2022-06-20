13b639c04abe5f235c15b6ef37a8dec6fa986e6d
kaysky4 <46075665+kaysky4@users.noreply.github.com>
2022-06-20 14:11:38 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -4,19 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-import BrowserRouter from 'react-router-dom/BrowserRouter'
-
-ReactDOM.render((
-   <BrowserRouter basename={process.env.PUBLIC_URL}>
-     <App />
-   </BrowserRouter>
-));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 reportWebVitals();
