07e8955e0c6deb826d05db87e762e3cb54dd2657
kaysky4 <46075665+kaysky4@users.noreply.github.com>
2022-07-08 17:23:09 -0500

    updating import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import SearchIcon from "./images/search.png";
 import TeamCard from "./Components/TeamCard";
