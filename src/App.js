e7fc9aa774df5c2f9f812326b2ff7f7f7ed71ebb
kaysky4 <46075665+kaysky4@users.noreply.github.com>
2022-07-08 16:15:04 -0500

    Adding Carousel, adding 2 additional Soccer teams, added responsiveness for mobile vs desktop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import SearchIcon from './images/search.png'
-import TeamCard from './Components/TeamCard';
-import React from 'react';
-import TeamData from './TeamData.json';
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./images/search.png";
+import TeamCard from "./Components/TeamCard";
+import TeamData from "./TeamData.json";
 
 //const API_URL = 'https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?s=Soccer&c=United_States';
 
 const App = () => {
-  const [teams, setTeams] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [teams, setTeams] = useState([TeamData]);
+  const [searchTerm, setSearchTerm] = useState([]);
 
   const searchTeams = async (strTeam) => {
     // const response = await fetch(`${TeamData}&s=${strTeam}`);
@@ -17,62 +16,64 @@ const App = () => {
 
     const data = TeamData.map((val, key) => {
       return teams.strTeam;
-    })
+    });
     setTeams(data.Search);
     console.log(data.Search);
-  }
+  };
 
-  useEffect(() => {
-    searchTeams('Austin FC');
-  },[]);
+  const slideLeft = () => {
+    let slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
 
+  const slideRight = () => {
+    let slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
 
   return (
     <div className="app">
-      <h1>MLS Teams</h1>
+      <h1>American Soccer Teams</h1>
 
-
-      <div className='search'>
-        <input 
-          placeholder='Search for teams'
+      <div className="search">
+        <input
+          placeholder="Search for teams"
           value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)} />
-        <img 
-        src={SearchIcon}
-        alt="search"
-        onClick={() => searchTeams(searchTerm)}/>
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchTeams(searchTerm)}
+        />
       </div>
 
-      {/* <div>
-        <button className='all-button'>View All Teams</button>
-      </div> */}
+      <div className="container">
+        <div className="carousel-outside">
+          <div className="carousel-btn" onClick={slideLeft}>
+            ❰
+          </div>
+          <div id="slider" className="carousel-inside">
+            {TeamData.filter((val) => {
+              if (searchTerm == "") {
+                return val;
+              } else if (
+                val.strTeam.toLowerCase().includes(searchTerm.toLowerCase())
+              ) {
+                return val;
+              }
+            }).map((val, key) => {
+              return <TeamCard team={val} />;
+            })}
+          </div>
 
-      <div className='container'>
-        {TeamData.filter((val)=> {
-          if (searchTerm == "") {
-            return val
-          } else if (val.strTeam.toLowerCase().includes(searchTerm.toLowerCase())) {
-            return val
-          }
-        }).map((val, key) => {
-          return <TeamCard team={val}/>
-        })}
+          <div className="carousel-btn" onClick={slideRight}>
+            ❱
+          </div>
+        </div>
       </div>
-      {/* {teams?.length > 0 
-        ? (
-          <div className='container'>
-            {TeamData.map((team, key) => (
-              <TeamCard team={team.strTeam}/>
-            ))}
-          </div>
-        ) : (
-          <div className='empty'>
-            <h2>No teams found</h2>
-          </div>
-        )
-      } */}
     </div>
   );
-}
+};
 
 export default App;
